05498dd1d0c212e9ed491011ef8908af40dedea9
Ch-Adrian <aderter116@gmail.com>
2021-10-11 21:16:03 +0200

    zad12

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -11,9 +11,16 @@ public class World {
 
     public static void run(String[] arr){
         System.out.println("Zwierzak idzie do przodu");
+
+        int cnt = 0;
         for(String str: arr){
-            System.out.print(str + ", ");
+            System.out.print(str);
+            if(cnt != arr.length - 1){
+                System.out.print(", ");
+            }
+            cnt++;
         }
+        System.out.println();
     }
 
 }
