43d2ff393697e39766ed2e9ef5d41e9122a44ab5
Ch-Adrian <aderter116@gmail.com>
2021-10-12 00:12:04 +0200

    zad25

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,55 +2,56 @@ package agh.ics.oop;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class World {
 
     public static void main(String[] args) {
         System.out.println("Start");
-        run(change(args));
+        run(args);
         System.out.println("Stop");
     }
 
-    public static Direction[] change(String[] arr){
-        Direction[] dir = new Direction[arr.length];
-
-        int i = 0;
-        for(String str: arr){
-            switch (str) {
-                case "f" -> dir[i] = Direction.FORWARD;
-                case "b" -> dir[i] = Direction.BACKWARD;
-                case "r" -> dir[i] = Direction.RIGHT;
-                case "l" -> dir[i] = Direction.LEFT;
-                default -> i--;
-            }
-            i++;
-        }
-        return Arrays.copyOfRange(dir, 0, i);
-    }
-
-    public static void run(Direction[] arr){
+    public static void run(String[] arr){
         System.out.println("Zwierzak idzie do przodu");
 
-        for(Direction dir: arr){
-            switch (dir) {
-                case FORWARD -> System.out.println("Zwierzak idzie do przodu.");
-                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
-                case LEFT -> System.out.println("Zwierzak skręca w lewo");
-                default -> {
-                }
-            }
-        }
+        Object[] arrDir = Stream.of(arr).filter(str -> (str.equals("f") || str.equals("b") || str.equals("r") || str.equals("l")))
+                .peek(str ->
+                {
+                    switch (str) {
+                        case "f" -> System.out.println("Zwierzak idzie do przodu");
+                        case "b" -> System.out.println("Zwierzak idzie do tyłu");
+                        case "r" -> System.out.println("Zwierzak skręca w prawo");
+                        default -> System.out.println("Zwierzak skręca w lewo");
+                    }
+                }).toArray();
 
-        int cnt = 0;
-        for(Direction str: arr){
-            System.out.print(str);
-            if(cnt != arr.length - 1){
-                System.out.print(", ");
-            }
-            cnt++;
-        }
-        System.out.println();
+        long dl = Stream.of(arr).filter(str -> (str.equals("f") || str.equals("b") || str.equals("r") || str.equals("l"))).count();
+        Stream<Direction> stream = Stream.of(arr).filter(str -> (str.equals("f") || str.equals("b") || str.equals("r") || str.equals("l")))
+                .map(str ->
+                switch (str) {
+                        case "f" -> Direction.FORWARD;
+                        case "b" -> Direction.BACKWARD;
+                        case "r" -> Direction.RIGHT;
+                        case "l" -> Direction.LEFT;
+                        default ->  Direction.FORWARD;
+                });
+        stream = stream.limit(dl - 1).peek(str -> System.out.print(str + ", "));
+
+        Direction last = Stream.of(arr).filter(str -> (str.equals("f") || str.equals("b") || str.equals("r") || str.equals("l")))
+                .map(str ->
+                        switch (str) {
+                            case "f" -> Direction.FORWARD;
+                            case "b" -> Direction.BACKWARD;
+                            case "r" -> Direction.RIGHT;
+                            case "l" -> Direction.LEFT;
+                            default -> Direction.FORWARD;
+                        }).skip(dl - 1).collect(Collectors.toList()).get(0);
+        Object[] dir = stream.toArray();
+        System.out.println(last);
     }
 
 }
