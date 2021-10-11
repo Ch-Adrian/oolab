677193ae5e659a32dc06e4fca0ed64a8d9f3af72
Ch-Adrian <aderter116@gmail.com>
2021-10-11 22:07:37 +0200

    zad17

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,38 +1,49 @@
 package agh.ics.oop;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class World {
-    @SuppressWarnings("EnhancedSwitchMigration")
+
     public static void main(String[] args) {
         System.out.println("Start");
-        //run(new String[]{"1", "2", "3"});
-
-        for(String dir: args){
-            switch(dir){
-                case "f":
-                    System.out.println("Zwierzak idzie do przodu.");
-                    break;
-                case "b":
-                    System.out.println("Zwierzak idzie do tyłu");
-                    break;
-                case "r":
-                    System.out.println("Zwierzak skręca w prawo");
-                    break;
-                case "l":
-                    System.out.println("Zwierzak skręca w lewo");
-                    break;
-                default:
-
-            }
-        }
-
+        run(change(args));
         System.out.println("Stop");
     }
 
-    public static void run(String[] arr){
+    public static Direction[] change(String[] arr){
+        Direction[] dir = new Direction[arr.length];
+
+        int i = 0;
+        for(String str: arr){
+            switch (str) {
+                case "f" -> dir[i] = Direction.FORWARD;
+                case "b" -> dir[i] = Direction.BACKWARD;
+                case "r" -> dir[i] = Direction.RIGHT;
+                case "l" -> dir[i] = Direction.LEFT;
+                default -> i--;
+            }
+            i++;
+        }
+        return Arrays.copyOfRange(dir, 0, i);
+    }
+
+    public static void run(Direction[] arr){
         System.out.println("Zwierzak idzie do przodu");
 
+        for(Direction dir: arr){
+            switch (dir) {
+                case FORWARD -> System.out.println("Zwierzak idzie do przodu.");
+                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
+                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
+                case LEFT -> System.out.println("Zwierzak skręca w lewo");
+                default -> {
+                }
+            }
+        }
+
         int cnt = 0;
-        for(String str: arr){
+        for(Direction str: arr){
             System.out.print(str);
             if(cnt != arr.length - 1){
                 System.out.print(", ");
