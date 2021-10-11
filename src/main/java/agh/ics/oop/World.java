69f9b6eeb5484a9d49f64123fc21da4a048081c1
Ch-Adrian <aderter116@gmail.com>
2021-10-11 21:13:49 +0200

    zad10/11

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,13 +4,16 @@ public class World {
     public static void main(String[] args) {
         System.out.println("Start systemu");
 
-        run();
+        run(new String[]{"1", "2", "3"});
 
         System.out.println("Koniec systemu");
     }
 
-    public static void run(){
+    public static void run(String[] arr){
         System.out.println("Zwierzak idzie do przodu");
+        for(String str: arr){
+            System.out.println(str);
+        }
     }
 
 }
