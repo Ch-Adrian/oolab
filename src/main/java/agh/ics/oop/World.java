a97e90922d7036edd5cd717b304fc1581eb7f5c3
Ch-Adrian <aderter116@gmail.com>
2021-10-11 21:07:06 +0200

    zad5

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,6 +2,8 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
+        System.out.println("Start systemu");
 
+        System.out.println("Koniec systemu");
     }
 }
