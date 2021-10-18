8f4fe8e1cb96707573f195553130ff12b1ccc8b4
Ch-Adrian <aderter116@gmail.com>
2021-10-18 23:00:58 +0200

    Lab2 zad8

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -12,6 +12,13 @@ public class World {
 
     public static void main(String[] args) {
 
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+
         /*Vector2d v = new Vector2d(1,2);
         Vector2d v2 = new Vector2d(1,2);
         System.out.println(v.equals(v2));*/
