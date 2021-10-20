fe0111532a12e4425976568d1f3e171eb73a355e
Ch-Adrian <aderter116@gmail.com>
2021-10-20 10:12:46 +0200

    Lab2 zad3/4 Testy

--- src/test/java/agh/ics/oop/MapDirectionTest.java
+++ src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -1,11 +1,15 @@
 package agh.ics.oop;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MapDirectionTest {
 
     @Test
     public void testNext(){
-        
+        assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
+        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
+        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
+        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
     }
 }
