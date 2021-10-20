35183cfe30de21fc3ab98d7a0d1d662879179d2f
Ch-Adrian <aderter116@gmail.com>
2021-10-20 10:21:47 +0200

    Lab2 zad5 Testy

--- src/test/java/agh/ics/oop/MapDirectionTest.java
+++ src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -12,4 +12,12 @@ public class MapDirectionTest {
         assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
         assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
     }
+
+    @Test
+    public void testPrevious(){
+        assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
+        assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
+        assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
+    }
 }
